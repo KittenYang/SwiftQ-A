c9b32c08a9280a1a4d20258d19c62e75f6b5f42e
KittenYang <imkittenyang@gmail.com>
2015-04-02 11:57:11 +0800

    updated

--- LearnPlayground.playground/section-1.swift
+++ LearnPlayground.playground/section-1.swift
@@ -203,33 +203,39 @@ returnFifteen()
 
 
 //-------函数作为另一个函数的返回值---------
-//(Void ->Int) 必须和 ()->Int 匹配
-func twoFuncs()->(Void ->Int){
-    func oneFun()->Int{
-        return 5
+//(Int ->Int) 必须和 (number: Int)->Int 格式匹配
+func makeIncrementer() -> (Int -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
     }
-    return oneFun
+    return addOne
+}
+var increment = makeIncrementer()
+increment(7)
+
+//--------函数作为参数传入另一个函数----------
+func hasAnyMatches(list:[Int],condition:Int->Bool)->Bool{
+    for item in list{
+        if condition(item){
+            return true
+        }
+    }
+    return false
 }
 
-var test = twoFuncs()
-test()
-
-
-//--------函数作为参数传入另一个函数--------
-func hasAnyMatches(list:[Int],condition:Int->Bool) -> Bool{
-    
+func lessThanTen(number:Int)->Bool{
+    return number < 10
 }
 
+var numbers = [20,19,7,12]
+hasAnyMatches(numbers, lessThanTen)
 
+numbers.map {
+    (number:Int) -> Int in
+    let result = 3 * number
+    return result
+}
 
-
-
-
-
-
-
-
-
-
+//--------
 
 
